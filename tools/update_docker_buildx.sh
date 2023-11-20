8f72120f65cdd78974fe609024cb73a2b2537301
Will Shanks <wshaos@posteo.net>
2023-11-20 12:42:08 -0500

    Add rootless docker

---
+++ tools/update_docker_buildx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+defaults="roles/host/defaults/main.yml"
+if [[ ! -f "$defaults" ]]; then
+    echo "Could not find defaults file ($defaults). Wrong working directory?" >&2
+    exit 1
+fi
+
+read -r -d '' PYCODE <<'EOF'
+import json
+import re
+import sys
+
+releases = json.loads(sys.stdin.read())
+releases = [r['tag_name'][1:] for r in releases if re.match('v[\d+\.]+$', r['tag_name'])]
+release = max(releases, key=lambda x: tuple(int(p) for p in x.split('.')))
+print(release)
+EOF
+
+buildx_version="$(curl -L https://api.github.com/repos/docker/buildx/releases | python -c "$PYCODE")"
+
+sed -i "s/^docker_buildx_version\:.*/docker_buildx_version: '$buildx_version'/" "$defaults"
+echo "Docker buildx version is ${buildx_version}"
